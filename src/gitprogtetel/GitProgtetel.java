b57824674db9b0300c9f29205741d5d6e06da5b8
PatrikB <bekpatrik24@gmail.com>
2024-05-25 09:56:16 +0200

    tomb es osszegzes

--- src/gitprogtetel/GitProgtetel.java
+++ src/gitprogtetel/GitProgtetel.java
@@ -5,7 +5,7 @@ public class GitProgtetel {
     public static void main(String[] args) {
         int[] szamok = {3, 5, 2, 8, 6, 1};
         legnagyobbkivalasztas(szamok);
-        
+        osszegzes();
     }
 
     private static void legnagyobbkivalasztas(int szamok[]) {
@@ -18,6 +18,30 @@ public class GitProgtetel {
         System.out.println("A legnagyobb szám a tömbben: " + max);
     }
 
-    
+    private static void osszegzes() {
+        int[] sorozat = {2, 5, 1, 6, 3};
+        kiirEredmeny("A sorozat elemei: ");
+        kiirTomb(sorozat);
+        int osszeg = 0;
+        //tétel kezdete
+        for (int i = 0; i < sorozat.length; i++) {
+            osszeg += sorozat[i];
+        }
+        //tétel vége
+        System.out.println("A sorozat összege: " + osszeg);
+    }
 
+     private static void kiirEredmeny(String eredmeny) {
+        System.out.println(eredmeny);}
+        
+        
+     private static void kiirTomb(int[] tomb) {
+
+        for (int i = 0; i < tomb.length; i++) {
+            System.out.print(tomb[i] + " ");
+        }
+        System.out.println("");
+    }
+    
+    
 }
